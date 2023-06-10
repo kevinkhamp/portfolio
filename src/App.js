b4d70f90fdcfc434102ddc661119c3ad3d178721
Kevin <khampkevin@gmail.com>
2023-06-10 14:22:25 -0400

    see change in ghp

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
     return (
         <div className='react-app'>
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react'
+import {Navbar, Container, Button, Nav} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import {DiCode} from 'react-icons'
+
+function NavBar() {
+    const 
+}

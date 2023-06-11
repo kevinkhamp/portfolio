4b894d6ff0e2085562e2ebf396ce3aa9787fd252
Kevin <khampkevin@gmail.com>
2023-06-11 19:47:16 -0400

    Can compile

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import About from './components/About'
 
 function App() {
     return (
-        <Router>
-        <>
-          <Navbar />
-          <Routes>
-            <Route />
-          </Routes>
-        </>
-      </Router> 
+        <div className='portfolio-app'>
+            <About />
+        </div>
     )
 }
+
+export default App
---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function About() {
+    return (
+        <div className='about-cont'>
+            <div className='about'>
+                <h1>Kevin Khamphounvong</h1>
+            </div>
+        </div>
+    )
+}
+
+export default About

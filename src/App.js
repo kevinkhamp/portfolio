3a871361323061367f5029847b1fa9fbf5a74569
Kevin <khampkevin@gmail.com>
2023-06-14 20:54:24 -0400

    so far so good

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header'
+import Navbar from './components/Navbar/Navbar';
+import About from './components/About/About';
+import Experience from './components/Experience/Experience';
+import Contact from './components/Contact/Contact';
+import Portfolio from './components/Portfolio/Portfolio';
 
 function App() {
   return (
     <>
       <Header />
+      <Navbar />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
     </>
   );
 }
